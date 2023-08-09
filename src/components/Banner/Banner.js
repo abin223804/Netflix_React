8380b726c611412ccf925e5c4967956ea13b816f
Abin s chandran <abinschandran@gmail.com>
2023-08-09 17:31:49 +0530

    error n banner loading 23:35

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,38 +1,48 @@
-import React from 'react'
+
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../constants/axios'
 import './Banner.css'
-import { useEffect } from 'react'
+import React,{ useEffect,useState } from 'react'
 
 const Banner = () => {
-useEffect(() => {
-  first
+const [movie, setMovie] = useState([])
 
-  return () => {
-    second
-  }
+
+useEffect(() => {
+  axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+    console.log(response.data.results[0])
+    setMovie(response.data.results[0])
+  })
+
+ 
 }, [])
 
+return (
+  <div
+  style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+   className='banner'>
+  <div className='content'>
+          <h1 className='title'>{movie?movie.title:''}</h1>
+          <div className='banner_button'>
+              <button className='button'>Play</button>
+              <button className='button'>My list</button>
+
+          </div>
+          <h1 className='description'>
+          {movie?movie.overview:""}
+          </h1>
+
+      </div>
+      <div className="fade_bottom">
+         
+      </div>
+    
+  </div>
+)
 
 
-  return (
-    <div className='banner'>
-        <div className='content'>
-            <h1 className='title'>Movie Name</h1>
-            <div className='banner_button'>
-                <button className='button'>Play</button>
-                <button className='button'>My list</button>
 
-            </div>
-            <h1 className='description'>
-            Using Lorem ipsum to focus attention on graphic elements in a webpage design
-            </h1>
-
-        </div>
-        <div className="fade_bottom">
-           
-        </div>
-      
-    </div>
-  )
+  
 }
 
 export default Banner
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,3 +2,5 @@ export const baseUrl='https://api.themoviedb.org/3';
 
 export const API_KEY='488b871a3a315aeb9f72be23df9f454a'
 
+export const imageUrl='https://api.themoviedb.org/3'
+
