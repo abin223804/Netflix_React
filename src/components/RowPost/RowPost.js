70424c938d3a33739dec600eabee255ad02b043e
Abin s chandran <abinschandran@gmail.com>
2023-08-12 14:31:53 +0530

    rowposter added

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import './RowPost.css'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../constants/axios'
 
 function Rowpost() {
+const [movies, setMovies] = useState([])
+
+useEffect(() => {
+  axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
+    console.log(response.data);
+    setMovies(response.data.results);
+  }).catch(err => {
+
+    console.log(err);
+  })
+
+ 
+}, [])
+
+
+
+
+
   return (
     <div className='row'>
-      <h2>Title</h2>
+      <h2>Netflix Originals</h2>
       <div className="posters">
-        {/* <img src="https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w" alt="poster" /> */}
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-        <img className='poster' src="https://e1.pxfuel.com/desktop-wallpaper/49/663/desktop-wallpaper-new-movie-posters-hollywood-movie-2022.jpg" alt="poster" />
-
+       
+{movies.map((obj)=>
+<img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+)}
        
         
       </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const baseUrl='https://api.themoviedb.org/3';
+export const baseUrl = "https://api.themoviedb.org/3";
 
-export const API_KEY='488b871a3a315aeb9f72be23df9f454a'
+export const API_KEY = "488b871a3a315aeb9f72be23df9f454a";
 
-export const imageUrl='https://image.tmdb.org/t/p/original'
+export const imageUrl = "https://image.tmdb.org/t/p/original";
