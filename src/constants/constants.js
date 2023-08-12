53e6f594a074441117796bd4787a28e8f94a0f87
Abin s chandran <abinschandran@gmail.com>
2023-08-12 13:39:56 +0530

    banner problem solved

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,5 +2,4 @@ export const baseUrl='https://api.themoviedb.org/3';
 
 export const API_KEY='488b871a3a315aeb9f72be23df9f454a'
 
-export const imageUrl='https://api.themoviedb.org/3'
-
+export const imageUrl='https://image.tmdb.org/t/p/original'
